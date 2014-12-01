6e87a9f962ac063e4adc7631921644349d265357
Matthew Green <matthew.green@incopro.co.uk>
2014-12-01 13:00:56 +0000

    Username and password added to cassandra configuration

--- src/main/java/com/blu/es/cassandra/CassandraFactory.java
+++ src/main/java/com/blu/es/cassandra/CassandraFactory.java
@@ -6,9 +6,14 @@ import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
 import com.datastax.driver.core.policies.LoadBalancingPolicy;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
 import java.util.*;
 
 /**
@@ -21,12 +26,13 @@ public class CassandraFactory {
     private static Session session;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CassandraFactory.class);
+    
     private static final String DATA_TYPE_TIMESTAMP="timestamp";
     private static final String DATA_TYPE_BOOLEAN="boolean";
 
 
     private CassandraFactory() {    }
-    private CassandraFactory(String keySpaceName, String hostName, String port, String dcName){
+    private CassandraFactory(String keySpaceName, String hostName, String port, String dcName, String username, String password){
         LoadBalancingPolicy loadBalancingPolicy = new DCAwareRoundRobinPolicy(dcName,2);
         PoolingOptions poolingOptions = new PoolingOptions();
 
@@ -34,11 +40,14 @@ public class CassandraFactory {
         poolingOptions.setMaxConnectionsPerHost(HostDistance.LOCAL, 50);
 
         //cluster = Cluster.builder().addContactPoints(hostName).withCompression(ProtocolOptions.Compression.SNAPPY)
-        cluster = Cluster.builder().addContactPoints(hostName)
+        Cluster.Builder builder = Cluster.builder().addContactPoints(hostName)
                 .withPoolingOptions(poolingOptions)
                 .withReconnectionPolicy(new ConstantReconnectionPolicy(100L))
-                .withLoadBalancingPolicy(loadBalancingPolicy)
-                .build();
+                .withLoadBalancingPolicy(loadBalancingPolicy);
+        if(!username.isEmpty() && !password.isEmpty()) {
+            builder.withCredentials(username, password);
+        }
+        cluster = builder.build();
         Metadata metadata = cluster.getMetadata();
         LOGGER.info("Connected to cluster: {}", metadata.getClusterName());
         for ( Host host : metadata.getAllHosts() ) {
@@ -48,8 +57,8 @@ public class CassandraFactory {
         LOGGER.info("Connection established!");
     }
 
-    public static CassandraFactory getInstance(String keySpaceName, String hostName, String port, String dcName){
-        return new CassandraFactory(keySpaceName, hostName, port, dcName);
+    public static CassandraFactory getInstance(String keySpaceName, String hostName, String port, String dcName, String username, String password){
+        return new CassandraFactory(keySpaceName, hostName, port, dcName, username, password);
     }
 
     public Session getSession() {
@@ -69,6 +78,7 @@ public class CassandraFactory {
         if (dataType == null){
             return value;
         }
+
         if(dataType.getName().toString().equalsIgnoreCase(DATA_TYPE_TIMESTAMP)){
             Date date = row.getDate(columnName);
             value = date!=null ? date.toString() :"";
@@ -78,9 +88,9 @@ public class CassandraFactory {
         } else{
             value = row.getString(columnName);
         }
-
         return value;
     }
+
     private String[] getHostArray(String propertiesVal){
         Iterable<String> values = Splitter.on(",").trimResults().split(propertiesVal);
 
